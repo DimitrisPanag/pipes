3b3b73725c7a1e3c3c50fc60bd6a210f31e86aa2
Dimitris Panagiotou <jim.panag2000@gmail.com>
2023-11-30 19:26:11 +0200

    New Project called Payment form working with angular pipes (named it pipes)

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  name!: string;
+
+  onNameChange(target: EventTarget){
+    const value = (<HTMLInputElement>target).value;
+    this.name = value;
+  }
+}
